2a6a80b42cd21c9809fca6d796da0dcc39cb3f0b
aayushwrld <142794224+aayushwrld@users.noreply.github.com>
2023-10-02 19:20:05 +0530

    Add files via upload

---
+++ finalgame.py
@@ -0,0 +1,79 @@
+import random
+
+def die_roll1():
+    return random.randint(1,6)
+
+def die_roll2():
+    return random.randint(1,6)
+
+sandl = {
+        
+        }
+
+pos_p1 = 0
+pos_p2 = 0
+
+def pos_change(name, position, die):
+    pos_updated = position + die
+    if pos_updated in sandl:
+        if sandl[pos_updated] > pos_updated:
+            print(name, "landed on a Ladder 🪜 ! goes up to", sandl[pos_updated])
+        elif sandl[pos_updated] < pos_updated:
+            print(name, "landed on a Snake 🐍 ! goes down to", sandl[pos_updated])
+        return sandl[pos_updated]
+    elif pos_updated > 100:
+        print(name, "has rolled over 100. Hence staying at the same position. ")
+        return position
+    else:
+        return pos_updated
+print("Welcome to Snakes 🐍 and Ladders 🪜! 😄")
+input("Press Enter key to start playing!")
+name1 = input("Enter name of Player 1  - ")
+name2 = input("Enter name of Player 2 - ")
+
+while True:
+    input("Press Enter to roll the die 🎲 -> ")
+
+    die1 = die_roll1()
+    if die1 == 6:
+        temp = 0
+        temp = temp + die1
+        die1 = die_roll1()
+        pos_p1 = temp + pos_p1
+        if pos_p1 == 100:
+            pass
+        else:
+            print(name1, "rolled 6!")
+            print("Rolling again,", name1, "got", die1)
+            pos_p1 = pos_change(name1, pos_p1, die1)
+            print(name1, "is now at position", pos_p1)
+    else:
+        print(name1, "rolled", die1)
+        pos_p1 = pos_change(name1, pos_p1, die1)
+        print(name1, "is now at position", pos_p1)
+
+    if pos_p1 == 100:
+        print(name1, "wins! 👑")
+        break
+
+    die2 = die_roll2()
+    if die2 == 6:
+        temp = 0
+        temp = temp + die2
+        die2 = die_roll1()
+        pos_p2 = temp + pos_p2
+        if pos_p2 == 100:
+            pass
+        else:
+            print(name2, "rolled 6!")
+            print("Rolling again,", name2, "got", die2)
+            pos_p2 = pos_change(name2, pos_p2, die2)
+            print(name2, "is now at position", pos_p2)
+    else:
+        print(name2, "rolled", die2)
+        pos_p2 = pos_change(name2, pos_p2, die2)
+        print(name2, "is now at position", pos_p2)
+
+    if pos_p2 == 100:
+        print(name2, "wins! 👑")
+        break
